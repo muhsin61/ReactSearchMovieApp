65f52d198ccdf755f157232f2dd0321d918d34c4
muhsin61 <muhsin_61_67@hotmail.com>
2020-07-29 23:02:39 +0300

    some css

--- src/movieSearch.js
+++ src/movieSearch.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import './App.css';
 
 export default function MovieSearch(){
     const [inputs,setInput] = useState("");
@@ -7,7 +8,7 @@ export default function MovieSearch(){
     const searchMovies = async (e) =>{
         e.preventDefault();
         if(inputs != ""){
-            await fetch(`https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${inputs}&page=1&include_adult=false`)
+            await fetch(`https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=tr-TR&query=${inputs}&page=1&include_adult=false`)
             .then(response => response.json())
             .then(data =>{ 
                 console.log(data)
@@ -29,19 +30,18 @@ export default function MovieSearch(){
                 {
 
                     movies.map(movie => (
-                      <div key={movie.id}>
-                        <h5>{movie.title}</h5>
-                        <p>{movie.overview}</p>
-                        <p>{movie.release_date}</p>
-                        <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title + ' poster'}></img>
-                        <hr></hr>
-                        </div>  
+                     <div key={movie.id} className="card">
+                        <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}></img>
+                        <div className="text">
+                            <h5>{movie.title}</h5>
+                            <p>{movie.overview}</p>
+                            <p>{movie.release_date}</p>
+                        </div>
+                     </div>  
                     )
                     
                     )
-                    
-                   
-                    
+ 
                 }
             </div>
         </div>
